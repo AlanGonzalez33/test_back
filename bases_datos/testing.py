f3cf45c381b2304ce9bdea90d4e4cd959cb17c84
NetAcad DEVASC <changeme@example.com>
2024-02-14 03:44:57 +0000

    avance 2 puertos

--- bases_datos/testing.py
+++ bases_datos/testing.py
@@ -42,4 +42,4 @@ def obtener_usuarios():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=True)
