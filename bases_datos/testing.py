8c14d0d44f1edb2313f1ebf569bd9e4c9fd1152a
root <root@labvm.vm>
2024-02-20 03:59:40 +0000

    version 3 QA 20 02 24

--- bases_datos/testing.py
+++ bases_datos/testing.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, jsonify
 import mysql.connector
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://Alan:Agonzalez3@localhost/Data_base_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://Alan:Agonzalez3@localhost/Base_Datos_beta2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -12,6 +12,8 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False)
+    contraseña = db.Column('contraseña', db.String(10), nullable=False)
+
 
 @app.route('/')
 def index():
@@ -25,19 +27,20 @@ def agregar_usuario():
     if request.method == 'POST':
         nombre = request.form['nombre']
         email = request.form['email']
+        contraseña = request.form['contraseña']
 
-        nuevo_usuario = Usuario(nombre=nombre, email=email)
+        nuevo_usuario = Usuario(nombre=nombre, email=email, contraseña=contraseña)
         db.session.add(nuevo_usuario)
         db.session.commit()
 
         return "Usuario agregado correctamente"
-@app.route('/api/usuarios', methods=['GET'])
-def obtener_usuarios():
-    # Obtener todos los usuarios desde la base de datos y convertirlos a formato JSON
-    usuarios = Usuario.query.all()
-    usuarios_json = [{'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email} for usuario in usuarios]
+#@app.route('/api/usuarios', methods=['GET'])
+#def obtener_usuarios():
+#    # Obtener todos los usuarios desde la base de datos y convertirlos a formato JSON
+#    usuarios = Usuario.query.all()
+#    usuarios_json = [{'id': usuario.id, 'nombre': usuario.nombre, 'email': usuario.email, 'contraseña':usuario.contraseña} for usuario in usuarios]
     
-    return jsonify({'usuarios': usuarios_json})
+#    return jsonify({'usuarios': usuarios_json})
 
 if __name__ == '__main__':
     with app.app_context():
